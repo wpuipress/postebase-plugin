6347b5aa45bf264ac02923af2dfb7fbba0463b24
wpuipress <120018841+wpuipress@users.noreply.github.com>
2024-04-11 20:18:30 +0100

    Bumped ver num

--- postebase.php
+++ postebase.php
@@ -6,7 +6,7 @@
  * Plugin Name: Postebase
  * Plugin URI: https://postebase.com
  * Description: Postebase companion plugin. Add's extra details to rest responses and expands rest api to work with the postebase platform.
- * Version: 0.0.6
+ * Version: 0.0.8
  * Requires at least: 6.4
  * Tested up to: 6.5.2
  * Requires PHP: 7.4
