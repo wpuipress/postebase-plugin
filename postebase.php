55ac035677c34b446fc874adb4a28e925fc391ba
wpuipress <120018841+wpuipress@users.noreply.github.com>
2024-04-11 10:40:20 +0100

    added meta

---
+++ postebase.php
@@ -0,0 +1,23 @@
+
+
+
+<?php
+/*
+Plugin Name: Postebase
+Plugin URI: https://postebase.com
+Description: Postebase helper plugin. Add's extra details to rest responses and expands rest api to work with the postebase platform.
+Version: 0.0.1
+Author: Postebase
+Text Domain: postebase
+Domain Path: /languages/
+*/
+
+// If this file is called directly, abort.
+!defined("ABSPATH") ? exit() : "";
+
+define("postebase_version", "0.0.1");
+define("postebase_path", plugin_dir_path(__FILE__));
+
+//require uip_plugin_path . "admin/vendor/autoload.php";
+//require uip_plugin_path . "admin/uipress-compiler.php";
+
